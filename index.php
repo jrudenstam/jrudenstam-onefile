3ccecb4e168008c8d3b3ae0896cb6aab570e482c
Jacob Rudenstam <jrudenstam@gmail.com>
2016-02-19 09:28:48 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Jacob Rudenstam</title>
+
+	<script src="https://use.typekit.net/zhm3nit.js"></script>
+	<script>try{Typekit.load({ async: true });}catch(e){}</script>
+
+	<style>
+		/**
+		 *
+		 *
+		 */
+
+		body {
+			font: 16px/1.45 "nimbus-sans", sans-serif; /* 300/400/700/900 */
+			max-width: 400px;
+			padding: 40px;
+		}
+
+		h1 {
+			font-family: "nimbus-sans-condensed", sans-serif; /* 400/700/900 */
+			font-size: 30px;
+			line-height: 1;
+			font-weight: 700;
+			margin: 0 0 .5em 0;
+		}
+
+		h1,
+		p,
+		ul {
+			opacity: 0;
+			transition: opacity .25s ease-in;
+		}
+
+		.wf-active h1,
+		.wf-active p,
+		.wf-active ul {
+			opacity: 1;
+		}
+
+		a,
+		a:visited {
+			color: #555;
+		}
+
+		ul {
+			list-style: none;
+			padding-left: 0;
+		}
+
+	</style>
+</head>
+<body>
+	<h1>Jacob Rudenstam (jrudenstam)</h1>
+	<p>I’m a freelance web developer and designer working with clients such as <a href="http://jkpg.com/">Destination Jönköping</a>, <a href="http://www.blastation.se/">Blå Station</a>, <a href="https://ju.se/">Jönköping University</a>, <a href="http://www.spotonsolutions.se/">SpotOn Solutions</a> and others.</p>
+	<ul>
+		<li><a href="mailto:jacob@jrudenstam.se">jacob@jrudenstam.se</a></li>
+		<li><a href="https://github.com/jrudenstam">jrudenstam</a></li>
+	</ul>
+</body>
+</html>
